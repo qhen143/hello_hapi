7aa8026e0f9815baa9fbb7f293ff854fb2fb7d37
qhen143 <38578819+qhen143@users.noreply.github.com>
2019-01-16 11:23:25 +1300

    Update hello_test.js

--- test/unit/hello_test.js
+++ test/unit/hello_test.js
@@ -6,6 +6,7 @@ const Hello = require('../../lib/hello.js');
 
 lab.experiment('test greetings', () => {
     // test
+    // test webhook
     lab.test('greets with name ', (done) => {
 
         Code.expect(Hello('Homer')).to.equal('Hello, Homer!');
